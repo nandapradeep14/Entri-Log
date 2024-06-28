7176deb64d7fff9751a114b63460aee1e621e55b
nandapradeep14 <nandapradeeppayeri@gmail.com>
2024-06-28 11:12:28 +0530

    project created

--- app/src/main/java/com/nanda/entrilog/MainActivity.java
+++ app/src/main/java/com/nanda/entrilog/MainActivity.java
@@ -1,21 +1,38 @@
 package com.nanda.entrilog;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText ed1,ed2;
+    AppCompatButton b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ed1=(EditText) findViewById(R.id.uname);
+        ed2=(EditText) findViewById(R.id.pass);
+        b1=(AppCompatButton) findViewById(R.id.logbtn);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String getUsername=ed1.getText().toString();
 
+                Toast.makeText(getApplicationContext(),getUsername,Toast.LENGTH_LONG).show();
+
+
+            }
         });
-    }
+            }
 }
